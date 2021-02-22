d31c6364d78721dc205b786c36afd6c844110e20
Ashen2b <2b.phnx@gmail.com>
2021-02-23 00:27:57 +0200

    Last stage implementation

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 <h1> SIMPLE SEARCH ENGINE </h1>
-HyperSkill project implementation. Stage 5/6.
+HyperSkill project implementation. Stage 6/6.
 Project implement some basic search algorithm with Inverted index search model. Accepts as a CLI arguments with flag "--data" followed by file name ex.: "--data myContactBook.txt".
+Added some context manager with interface implementation.
--- SearchEngine.java
+++ SearchEngine.java
@@ -32,11 +32,81 @@ class Person {
     }
 }
 
+class SelectionContext {
+
+    private SearchStrategyAlgorithm algorithm;
+
+    public void setAlgorithm(SearchStrategyAlgorithm algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    public ArrayList<String> searchPeople(String query) {
+        return this.algorithm.search(query);
+    }
+}
+
+interface SearchStrategyAlgorithm {
+
+    ArrayList<String> search(String query);
+}
+
+class SearchAllPeopleAlgorithm implements SearchStrategyAlgorithm {
+
+    @Override
+    public ArrayList<String> search(String query) {
+        var results = new ArrayList<String>();
+        if (SearchEngine.invertedIndexMap.containsKey(query)) {
+            ArrayList<Integer> lines = SearchEngine.invertedIndexMap.get(query);
+            for (int line : lines) {
+                results.add(SearchEngine.allPeopleLines.get(line));
+            }
+            return results;
+        } else {
+            return null;
+        }
+    }
+}
+
+class SearchAnyPeopleAlgorithm implements SearchStrategyAlgorithm {
+
+    @Override
+    public ArrayList<String> search (String query) {
+        var results = new HashSet<String>();
+        for (String word : query.split(" ")) {
+            if (SearchEngine.invertedIndexMap.containsKey(word)) {
+                ArrayList<Integer> lines = SearchEngine.invertedIndexMap.get(word);
+                for (int line : lines) {
+                    results.add(SearchEngine.allPeopleLines.get(line));
+                }
+            }
+        }
+        return new ArrayList<>(results);
+    }
+}
+
+class SearchNonePeopleAlgorithm implements SearchStrategyAlgorithm {
+
+    @Override
+    public ArrayList<String> search (String query) {
+        var results = new ArrayList<>(SearchEngine.allPeopleLines);
+        for (String word : query.split(" ")) {
+            if (SearchEngine.invertedIndexMap.containsKey(word)) {
+                ArrayList<Integer> ignoreLines = SearchEngine.invertedIndexMap.get(word);
+                for (int line : ignoreLines) {
+                    String toDelete = SearchEngine.allPeopleLines.get(line);
+                    results.remove(toDelete);
+                }
+            }
+        }
+        return results;
+    }
+}
+
 public class SearchEngine {
     private static final Scanner scanner = new Scanner(System.in);
     private static final ArrayList<Person> allPeopleObjects = new ArrayList<>();
-    private static final ArrayList<String> allPeopleLines = new ArrayList<>();
-    private static final Map<String, ArrayList<Integer>> invertedIndexMap = new HashMap<>();
+    protected static final ArrayList<String> allPeopleLines = new ArrayList<>();
+    protected static final Map<String, ArrayList<Integer>> invertedIndexMap = new HashMap<>();
 
     public static void main(String[] args) {
         try {
@@ -97,8 +167,10 @@ public class SearchEngine {
     }
 
     private static void searchMenu() {
+        System.out.println("Select a matching strategy: ALL, ANY, NONE");
+        String mode = scanner.nextLine();
         System.out.println("Enter a name or email to search all suitable people:");
-        ArrayList<String> results = searchQuery(scanner.nextLine().toUpperCase());
+        ArrayList<String> results = searchQuery(scanner.nextLine().toUpperCase(), mode);
         if (results == null) {
             System.out.println("No matching people found.");
         } else {
@@ -109,17 +181,19 @@ public class SearchEngine {
         }
     }
 
-    private static ArrayList<String> searchQuery(String query) {
-        var results = new ArrayList<String>();
-        if (invertedIndexMap.containsKey(query)) {
-            ArrayList<Integer> lines = invertedIndexMap.get(query);
-            for (int line : lines) {
-                results.add(allPeopleLines.get(line));
-            }
-            return results;
+    private static ArrayList<String> searchQuery(String query, String mode) {
+        SelectionContext context = new SelectionContext();
+        if ("ALL".equals(mode)) {
+            SearchAllPeopleAlgorithm algorithm = new SearchAllPeopleAlgorithm();
+            context.setAlgorithm(algorithm);
+        } else if ("ANY".equals(mode)) {
+            SearchAnyPeopleAlgorithm algorithm = new SearchAnyPeopleAlgorithm();
+            context.setAlgorithm(algorithm);
         } else {
-            return null;
+            SearchNonePeopleAlgorithm algorithm = new SearchNonePeopleAlgorithm();
+            context.setAlgorithm(algorithm);
         }
+        return context.searchPeople(query);
     }
 
     private static void displayAllPeople() {
