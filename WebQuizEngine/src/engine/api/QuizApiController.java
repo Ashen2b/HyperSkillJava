237ad27062c941d9f3c072fc21a7e14234787c40
Ashen2b <2b.phnx@gmail.com>
2021-04-09 12:32:18 +0300

    Initial commit to Web Quiz Engine

---
+++ WebQuizEngine/src/engine/api/QuizApiController.java
@@ -0,0 +1,8 @@
+package engine.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QuizApiController {
+    
+}
---
+++ WebQuizEngine/src/engine/service/QuizService.java
@@ -0,0 +1,8 @@
+package engine.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuizService {
+
+}
